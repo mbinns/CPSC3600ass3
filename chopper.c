2bdaaeb4460134563575d9befc0593e50d98b5fa
Chris Austin <acausti5043@gmail.com>
2016-10-19 23:14:51 -0400

    Add files via upload

---
+++ chopper.c
@@ -0,0 +1,105 @@
+#include "set.h"
+#include "common.h"
+
+
+struct config
+{
+    const char * port;
+	const char * message;
+	const char * servAdd;
+	int view;
+
+};
+
+int parse_args(int argc, char * argv[], struct config * cfg);
+void terminate(int signum);
+
+//Keep state information
+struct set st;
+struct addrinfo * addr_list;
+
+long long connections = 0;
+
+int main(int argc, char * argv[])
+{
+    //char buf[BUF_MAX];
+    
+    int ret;
+
+    struct config cfg;
+
+    // parse args into config structure
+    ret = parse_args(argc, argv, &cfg);
+    if(ret!=0)
+        return 1;
+
+}
+
+int parse_args(int argc, char * argv[], struct config * cfg)
+{
+    int usage = 0;
+
+    // show usage on zero arguments
+    if (argc == 1 || strcmp(argv[1], "-") == 0)
+        usage = 1;
+
+    // loop through all arguments with getopt and set variables as needed
+    int c;
+    while ((c = getopt(argc, argv, "p:s:va")) != -1)
+    {
+        switch (c)
+        {
+            case 'p':
+                cfg->port = optarg;
+                break;
+
+			case 'a':
+                cfg->message = optarg;
+                break;
+
+			case 's':
+                cfg->servAdd = optarg;
+                break;
+
+			case 'v':
+                cfg->view = 0;
+                break;
+
+            case '?':
+                usage = 1;
+                break;
+
+            default:
+                usage = 1;
+                break;
+        }
+    }
+
+    if (usage)
+    {
+        fprintf(stderr, "Usage: %s -p <port> -s <server> [-a <message>] [-v (http view call)]\n", argv[0]);
+        return 1;
+    }
+
+    return 0;
+}
+
+void terminate(int signum)
+{
+    printf("%lld\t", connections);
+
+    struct node * ptr = set_first(&st);
+    while(ptr != NULL)
+    {
+        printf("%s, ", set_data(ptr));
+        ptr = set_next(ptr);
+    }
+
+    printf("\n");
+
+    set_free(&st);
+
+    freeaddrinfo(addr_list);
+
+    exit(0);
+}
